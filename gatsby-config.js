b43dc0e6b5d276d4d99c6c3d8ca4e51bf1bb0070
Jona Schlegel <jona.schlegel@gmail.com>
2020-04-12 17:40:50 +0200

    Create initial version of site

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Jona Schlegel - landscape archaeologist',
+    author: 'Jona Schlegel',
+    description: 'Portfolio and Blog',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
+  ],
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => (
+  <div id="footer">
+    <div className="inner">
+      <ul className="icons">
+        <li>
+          <a
+            href="https://twitter.com/JonaSchlegel"
+            className="icon fa-twitter"
+          >
+            <span className="label">Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/jonaschlegel" className="icon fa-github">
+            <span className="label">GitHub</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Footer
